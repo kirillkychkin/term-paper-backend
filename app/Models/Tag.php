e92bf5e4bb5c937f75a773ca3966a67dae61e49c
Kirill Kychkin <kychkin.kirill@gmail.com>
2025-05-09 20:34:48 +0900

    add relations import in models

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tag extends Model
 {
