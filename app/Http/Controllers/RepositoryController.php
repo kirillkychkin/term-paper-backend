9b1cd8767dfaa74bd64869f12bfb34dc976a275a
Kirill Kychkin <kychkin.kirill@gmail.com>
2025-05-15 16:23:05 +0900

    fix order by

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -11,57 +11,58 @@ class RepositoryController extends Controller
 {
     public function search(Request $request)
     {
-        $tags = $request->input('tags');
-        $languages = $request->input('languages');
+        $tags = $request->input('tags', []);
+        $languages = $request->input('languages', []);
 
-        $query = Repository::query()        
-        ->with(['tags', 'languages']) 
-        ->withCount([
-            'tags as matching_tags_count' => function($query) use ($tags) {
-                if (!empty($tags)) {
-                    $query->whereIn('tags.id', $tags);
-                }
-            },
-            'languages as matching_languages_count' => function($query) use ($languages) {
-                if (!empty($languages)) {
-                    $query->whereIn('languages.id', $languages);
-                }
-            }
-        ])
-        ->addSelect([
-            'total_matches' => function($query) {
-                $query->selectRaw('(matching_tags_count + matching_languages_count)');
-            }
-        ]);
+        $query = Repository::query()->with(['tags', 'languages']);
 
-        // Apply OR condition if both filters are present
-        if (!empty($tags) && !empty($languages)) {
-            $query->where(function($q) use ($tags, $languages) {
-                $q->whereHas('tags', function($q) use ($tags) {
+        if (!empty($tags)) {
+            $query->withCount([
+                'tags as matching_tags_count' => function ($q) use ($tags) {
                     $q->whereIn('tags.id', $tags);
-                })
-                ->orWhereHas('languages', function($q) use ($languages) {
+                }
+            ]);
+        }
+
+        if (!empty($languages)) {
+            $query->withCount([
+                'languages as matching_languages_count' => function ($q) use ($languages) {
+                    $q->whereIn('languages.id', $languages);
+                }
+            ]);
+        }
+
+        if (!empty($tags) && !empty($languages)) {
+            $query->where(function ($q) use ($tags, $languages) {
+                $q->whereHas('tags', function ($q) use ($tags) {
+                    $q->whereIn('tags.id', $tags);
+                })->orWhereHas('languages', function ($q) use ($languages) {
                     $q->whereIn('languages.id', $languages);
                 });
             });
-        }
-        // Apply single conditions if only one filter is present
-        elseif (!empty($tags)) {
-            $query->whereHas('tags', function($q) use ($tags) {
+        } elseif (!empty($tags)) {
+            $query->whereHas('tags', function ($q) use ($tags) {
                 $q->whereIn('tags.id', $tags);
             });
-        }
-        elseif (!empty($languages)) {
-            $query->whereHas('languages', function($q) use ($languages) {
+        } elseif (!empty($languages)) {
+            $query->whereHas('languages', function ($q) use ($languages) {
                 $q->whereIn('languages.id', $languages);
             });
         }
-        
-        // If both arrays are empty, return all repositories
-        $repositories = $query->orderBy('total_matches', 'desc')->get();
-        
+
+        $repositories = $query->get();
+
+        foreach ($repositories as $repo) {
+            $repo->matching_tags_count = $repo->matching_tags_count ?? 0;
+            $repo->matching_languages_count = $repo->matching_languages_count ?? 0;
+            $repo->total_matches = $repo->matching_tags_count + $repo->matching_languages_count;
+        }
+
+        $repositories = $repositories->sortByDesc('total_matches')->values();
+
         return response()->json($repositories);
-    }    
+    }
+
 
     public function getOne(string $repositoryId) {
         
